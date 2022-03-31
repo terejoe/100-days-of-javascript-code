138482282e96719613f0bcfc9bb0c461e89aaaa0
Nzeamalu Nkechinyere Terejoe <nzeamalunkechinyere@gmail.com>
2022-03-31 11:37:00 +0100

    Tenth Day: Calculator Project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 100-days-of-javascript-code
-Day-9
-A program that pops up a modal box for a selected image and slides left or right through the images.
+Day-10
+A program  of a calculator.
--- scripts.js
+++ scripts.js
@@ -1,68 +1,24 @@
-//Modal Box
-var storeItems = document.querySelectorAll(".store-item");
-var modal = document.getElementById("modal");
-var closeBtn = document.querySelector(".close");
+let buttons = document.querySelectorAll(".btn");
+let display = document.querySelector("input");
 
-var images = document.querySelectorAll("img");
-var btnLeft = document.querySelector(".btnLeft");
-var btnRight = document.querySelector(".btnRight");
-var counter;
-var displayImg;
-var myArray = [];
-
-for (let i = 0; i < images.length; i++) {
-    myArray.push(images[i].src);
-}
-
-closeBtn.addEventListener("click", closeModal);
-
-btnLeft.addEventListener("click", myFunction);
-btnRight.addEventListener("click", myFunction);
+let numBtns = document.querySelectorAll("[data-num]");
+let equalBtn = document.querySelector(".btn-equal");
+let deleteBtn = document.querySelector(".btn-clear");
 
 
-storeItems.forEach((item) =>{
-    // item.addEventListener("click",openModal(item));
-    item.addEventListener("click", function() {
-        openModal(item);
+buttons.forEach((button) =>{
+    button.addEventListener("click", (event) =>{
+        let value = event.target.dataset.num;
+        display.value += value;
     })
-})
+});
 
-function openModal(item) {
-    modal.style.display = "block";
-    var img = item.childNodes[1].childNodes[1].childNodes[1].src;
-    
-    for(let i = 0; i < myArray.length; i++){
-        if(img === myArray[i]){
-            displayImg = myArray[i];
-            counter = i;
-        }
-    }
-    modal.childNodes[1].childNodes[3].style.background = `url(${displayImg})`;
-}
+deleteBtn.addEventListener("click", (event) => {
+    display.value = ""
+});
 
-function closeModal() {
-    modal.style.display = "none";
-}
+equalBtn.addEventListener("click", (event) => {
+    display.value = eval(display.value);
+});
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
-//Moving left and right
-function myFunction(e) {
-    if (e.target.classList.contains("fa-caret-right")) {
-        counter++;
-        if (counter > myArray.length-1) {
-            counter = 2;
-        }
-    } else if (e.target.classList.contains("fa-caret-left")) {
-        counter--;
-        if (counter < 2) {
-            counter = myArray.length -1;
-        }
-    }
-    modal.childNodes[1].childNodes[3].style.background = `url(${myArray[counter]})`;
-}
