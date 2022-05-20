a0ea2db77cf631fea811f27f912b09150b44a870
Nzeamalu Nkechinyere Terejoe <nzeamalunkechinyere@gmail.com>
2022-05-20 14:16:55 +0100

    Sixteenth Day: A flashcard project that stores questions and answers

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 100-days-of-javascript-code
-Day-15
-Course Form Project
+Day-16
+Flashcard Project
--- scripts.js
+++ scripts.js
@@ -1,104 +1,136 @@
-class Book {
-  constructor(name, course, author, img) {
-    this.name = name;
-    this.course = course;
-    this.author = author;
-    this.img = img;
+class Card {
+  constructor(question,answer) {
+    this.question = question;
+    this.answer = answer;
   }
 }
 
-let formControls = document.querySelectorAll(".form-control");
-let submitBtn = document.querySelector(".btn");
-
-
-// submitBtn.disabled = false;
-
-formControls.forEach(formControl => {
-  formControl.addEventListener("blur", () => {
-    if(formControl.value === ""){
-      formControl.classList.add("fail");
-      formControl.classList.remove("complete");
-    }else{
-      formControl.classList.add("complete");
-      formControl.classList.remove("fail");
-    }
-    validate()
-  });
-}); 
-
-function validate(){
-  
-  if ((formControls[0].classList.contains("complete")) && (formControls[1].classList.contains("complete")) && (formControls[2].classList.contains("complete"))) {
-    submitBtn.disabled = false;
-  }else{
-    submitBtn.disabled= true;
-  }
+//DOM Populate
+document.addEventListener("DOMContentLoaded", populateItems);
+function populateItems(){
+  let array = getItemsFromLocalStorage();
+  array.forEach(data =>{
+    setupCard(data);
+  })
 }
 
-//Adding the values of the input into the course form with an image
-function imgReload(){
-  let num = Math.floor(Math.random() * 6);
-  let img = `img/cust-${num}.jpg`;
-  return img;
-}
+//Opening the flashcard
+let showBtn = document.getElementById("show-btn");
+let showCard = document.querySelector(".question-card");
 
-function clearFields() {
-  formControls[0].value = "";
-  formControls[1].value = "";
-  formControls[2].value = "";
-}
-
-
-let load = document.querySelector(".loading");
-let feedback = document.querySelector(".feedback");
-submitBtn.addEventListener("click", (e) =>{
-  e.preventDefault();
-  load.style.display = "block";
-  feedback.style.display = "block";
-
-  setTimeout(() => {
-    load.style.display = "none";
-    feedback.style.display = "none";
-
-    runTemplate(book);
-
-    clearFields();
-
-  }, 2000);
-
-  let nameInput = formControls[0].value;
-  let courseInput = formControls[1].value;
-  let authorInput = formControls[2].value;
-  imgInput = imgReload();
-
-  const book = new Book(nameInput, courseInput, authorInput, imgInput);
-  
+showBtn.addEventListener("click", () =>{
+  showCard.style.display = "block";
 })
 
-function runTemplate(book) {
+//Closing the flashcard
+let closeBtn = document.querySelector(".close-btn");
+closeBtn.addEventListener("click", () =>{
+  showCard.style.display = "none";
+})
 
+//Displaying the feedback
+let submitBtn = document.querySelector(".submitBtn");
+
+submitBtn.addEventListener("click", (e)=>{
+  let feedback = document.querySelector(".feedback");
+  let questionInput = document.getElementById("question-input");
+  let answerInput = document.getElementById("answer-input");
+  
+  e.preventDefault()
+  if(answerInput.value == "" || questionInput.value == ""){
+    feedback.style.display = "block";
+  
+    setTimeout(() =>{
+      feedback.style.display = "none";
+    }, 2000)
+  }else{
+    let questionValue = questionInput.value;
+    let answerValue = answerInput.value;
+
+    const card = new Card(questionValue, answerValue);
+
+    setupCard(card);
+    addToLocalStorage(card)
+  }
+  // console.log(questionInput);
+  // console.log(answerInput);
+  questionInput.value = "";
+  answerInput.value = "";
+})
+
+function setupCard(card){
   let newElement = document.createElement("div");
-  newElement.classList.add("col-11", "mx-auto", "col-md-6", "col-lg-4", "my-3");
-  newElement.innerHTML = `<div class="card text-left">
-                  <img src="${book.img}" class="card-img-top" alt="">
-                  <div class="card-body">
-                    <!-- customer name -->
-                    <h6 class="text-capitalize "><span class="badge bg-warning mr-2">name :</span><span id="customer-name">
-                        ${book.name}</span></h6>
-                    <!-- end of customer name -->
-                    <!-- customer name -->
-                    <h6 class="text-capitalize my-3"><span class="badge bg-success mr-2">course :</span><span
-                        id="customer-course">
-                        ${book.course}
-                      </span></h6>
-                    <!-- end of customer name -->
-                    <!-- customer name -->
-                    <h6 class="text-capitalize"><span class="badge bg-danger mr-2">author :</span><span id="course-author">
-                        ${book.author}</span></h6>
-                    <!-- end of customer name -->
-                  </div>
-                </div>
-                <!-- single customer -->
-                `
-  document.querySelector(".customer-list").appendChild(newElement);
+  newElement.classList.add("col-md-4");
+  newElement.innerHTML = `<div class="card card-body flashcard my-3">
+         <h4 class="text-capitalize">${card.question}</h4>
+         <a href="#" class="text-capitalize my-3 show-answer">show/hide answer</a>
+         <h5 class="answer mb-3">${card.answer}</h5>
+         <div class="flashcard-btn d-flex justify-content-between">
+          <a href="#" id="edit-flashcard" class=" btn my-1 edit-flashcard text-uppercase" data-id="">edit</a>
+          <a href="#" id="delete-flashcard" class=" btn my-1 delete-flashcard text-uppercase">delete</a>
+         </div>`
+  document.querySelector("#questions-list").appendChild(newElement);
+}
+
+//Displaying and hiding answer
+let showLink = document.querySelector("#questions-list");
+
+showLink.addEventListener("click", (event) =>{
+  event.preventDefault();
+  let questionInput = document.getElementById("question-input");
+  let answerInput = document.getElementById("answer-input");
+  // let showAnswer = document.querySelector(".answer");
+  // console.log(event.target);
+  if (event.target.classList.contains("show-answer")) {
+    if (event.target.nextElementSibling.classList.contains('showItem')) {
+      event.target.nextElementSibling.classList.remove("showItem")
+    } else {
+      event.target.nextElementSibling.classList.add("showItem")
+    }
+  }
+
+  if (event.target.classList.contains("delete-flashcard")) {
+    event.target.parentElement.parentElement.parentElement.remove();
+    let item = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+    removeFromLocalStorage(item);
+  }
+
+  if (event.target.classList.contains("edit-flashcard")) {
+    event.target.parentElement.parentElement.parentElement.remove();
+    let editText = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+    questionInput.value = editText;
+    answerInput.value = "";
+    let item = event.target.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent
+    removeFromLocalStorage(item);
+  }
+})
+
+//Local storage
+
+//Getting items from the local storage
+function getItemsFromLocalStorage(){
+  let card = [];
+  let cardItems = localStorage.getItem("card");
+  if (cardItems) {
+    card = JSON.parse(localStorage.getItem("card"))
+  }
+  return card;
+}
+
+//Adding the items to the localstorage
+function addToLocalStorage(item) {
+  let arr = getItemsFromLocalStorage()
+  arr.push(item);
+  localStorage.setItem("card", JSON.stringify(arr));
+}
+
+//Removing an item
+function removeFromLocalStorage(item){
+  let arr = getItemsFromLocalStorage();
+  arr.forEach((element, i) => {
+    if (element.question === item) {
+      arr.splice(i,1)
+    }
+  })
+  localStorage.setItem("card", JSON.stringify(arr))
 }
